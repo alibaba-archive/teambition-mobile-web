f46ec278f737372e09add7a431f0b43d80bd26ba
Long Yinan <lynweklm@gmail.com>
2016-01-22 11:08:16 +0800

    fix(wechat/invite): fix class name

--- src/wechat/invited/InviteView.ts
+++ src/wechat/invited/InviteView.ts
@@ -1,10 +1,15 @@
 'use strict';
-import {View, ProjectsAPI} from '../index';
+import {inject, View, ProjectsAPI} from '../index';
 import {IProjectData} from 'teambition';
 
+@inject([
+  '$window',
+  'ProjectsAPI'
+])
 class InvitedView extends View {
 
   private ProjectsAPI: ProjectsAPI;
+  private $window: angular.IWindowService;
 
   constructor() {
     super();
@@ -15,19 +20,25 @@ class InvitedView extends View {
     const _projectId = this.$state.params.projectId;
     const _signCode = this.$state.params.signCode;
     const _invitorId = this.$state.params.inviterId;
-    return this.ProjectsAPI.joinByCode(_projectId, _signCode, _invitorId)
-    .then((data: IProjectData) => {
-      if (data.name) {
-        this.showMsg('success', '已经加入', data.name);
-      }else {
-        this.showMsg('success', '已经加入', '');
-      }
-    })
-    .catch((reason: any) => {
-      const message = this.getFailureReason(reason);
-      this.showMsg('error', '加入项目失败', message);
-    });
+    if (_projectId && _signCode && _invitorId) {
+      return this.ProjectsAPI.joinByCode(_projectId, _signCode, _invitorId)
+      .then((data: IProjectData) => {
+        if (data._id) {
+          this.$window.location.hash = `/project/${data._id}/home`;
+          const projectName = data.name ? data.name : '';
+          setTimeout(() => {
+            this.showMsg('success', '已经加入', projectName);
+          }, 500);
+        }else {
+          this.showMsg('error', '加入项目失败', '服务端返回数据错误');
+        }
+      })
+      .catch((reason: any) => {
+        const message = this.getFailureReason(reason);
+        this.showMsg('error', '加入项目失败', message);
+      });
+    }
   }
 }
 
-angular.module('teambition').service('InvitedView', InvitedView);
+angular.module('teambition').controller('InvitedView', InvitedView);
